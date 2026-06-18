e9362b33b4f0dee21e037fa88235f4ffe28ffaf4
AlenaKravtsova <babkaroza@gmail.com>
2026-06-18 09:12:35 +0200

    Added functionalities to budget routes to manage simple CRUD queries, added validations to all budget routes

--- backend/src/controllers/BudgetController.ts
+++ backend/src/controllers/BudgetController.ts
@@ -1,35 +1,91 @@
 import type { Request, Response } from "express";
 import Budget from "../models/Budget";
 
+type BudgetParams = {
+  id: string;
+};
+
 export class BudgetController {
   static getAllBudgets = async (req: Request, res: Response) => {
-    console.log("Desde BudgetController getAll");
+    try {
+      //aqui budget es la tabla entera solo la consultamos
+      const budgets = await Budget.findAll({
+        order: [["createdAt", "DESC"]],
+      });
+      res.json({ budgets });
+    } catch (error) {
+      res
+        .status(500)
+        .json({ error: "Ha ocurrido un error al obtener los presupuestos" });
+    }
   };
 
   static createBudget = async (req: Request, res: Response) => {
     try {
-        //creamos un modelo en memoria
-        const budget = new Budget(req.body);
-        //almacenamos el modelo en memoria en la BD
-        await budget.save();
+      //creamos un modelo en memoria, un objeto = fila, es como preparar el INSERT antes de lanzar la quert
+      const budget = new Budget(req.body);
+      console.log(budget);
+      //almacenamos el modelo en memoria en la BD
+      await budget.save();
 
-        res.status(201).json({message: 'Presupuesto creado correctamente'})
-
+      res.status(201).json({ message: "Presupuesto creado correctamente" });
     } catch (error) {
-        res.status(500).json({error: 'Ha ocurrido un error'})
+      res
+        .status(500)
+        .json({ error: "Ha ocurrido un error al crear el presupuesto" });
     }
   };
 
-  static getBudgetById = async (req: Request, res: Response) => {
-    console.log("Desde BudgetController getByID");
+  static getBudgetById = async (req: Request<BudgetParams>, res: Response) => {
+    try {
+      const { id } = req.params;
+      const budget = await Budget.findByPk(id);
+
+      if (!budget) {
+        res.status(404).json({ error: "No existe presupuesto con este ID" });
+      }
+      res.status(200).json({ budget });
+    } catch (error) {
+      res
+        .status(500)
+        .json({ error: "Ha ocurrido un error al recuperar el presupuesto" });
+    }
   };
 
-static updateBudgetById = async (req: Request, res: Response) => {
-    console.log("Desde BudgetController editBudget");
+  static updateBudgetById = async (
+    req: Request<BudgetParams>,
+    res: Response,
+  ) => {
+    try {
+      const { id } = req.params;
+      const budget = await Budget.findByPk(id);
+      if (!budget) {
+        res.status(404).json({ error: "No existe presupuesto con este ID" });
+      }
+      await budget.update(req.body);
+      res.status(201).json({message: 'Regsitro actualizado correctamente'})
+    } catch (error) {
+      res
+        .status(500)
+        .json({ error: "Ha ocurrido un error al actualizar el presupuesto" });
+    }
   };
 
-  static deleteBudget = async (req: Request, res: Response) => {
-    console.log("Desde BudgetController deleteBudget");
-  };
+  static deleteBudget = async (req: Request<BudgetParams>, res: Response) => {
+    try {
+    const { id } = req.params;
+      const budget = await Budget.findByPk(id);
 
+      if (!budget) {
+        res.status(404).json({ error: "No existe presupuesto con este ID" });
+      }
+      await budget.destroy();
+      res.status(201).json({message: 'Regsitro borrado correctamente'})
+
+    } catch (error) {
+        res
+        .status(500)
+        .json({ error: "Ha ocurrido un error al borrar el presupuesto" });
+    }
+  };
 }
--- backend/src/routes/budgetRoutes.ts
+++ backend/src/routes/budgetRoutes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { BudgetController } from "../controllers/BudgetController";
-import { body } from 'express-validator'
+import { body, param } from 'express-validator'
 import { handleInputErrors } from "../middleware/validation";
 
 const router = Router();
@@ -18,8 +18,30 @@ router.post('/',
     BudgetController.createBudget)
 
 
-router.get('/:id', BudgetController.getBudgetById)
-router.put('/:id', BudgetController.updateBudgetById)
-router.delete('/:id', BudgetController.deleteBudget)
+router.get('/:id',
+    param('id').isInt().withMessage('ID no válido')
+    .custom((value) => value > 0).withMessage('ID no válido'),
+    handleInputErrors,
+    BudgetController.getBudgetById)
+
+router.put('/:id', 
+    param('id').isInt().withMessage('ID no válido')
+    .custom((value) => value > 0).withMessage('ID no válido'),
+    body('name')
+    .notEmpty().withMessage('El nombre no puede ir vacío'),
+    body('amount')
+    .notEmpty().withMessage('La cantidad no puede ir vacía')
+    .isNumeric().withMessage('La cantidad debe ser un valor numérico')
+    //evaluamos el valor de ese campo, el callback debe evaluarse en FALSE para ejecutarse
+    .custom((value) => value > 0).withMessage('El valor numérico debe ser positivo'),
+    handleInputErrors,
+    BudgetController.updateBudgetById)
+
+
+router.delete('/:id', 
+    param('id').isInt().withMessage('ID no válido')
+    .custom((value) => value > 0).withMessage('ID no válido'),
+    handleInputErrors,
+    BudgetController.deleteBudget)
 
 export default router;
