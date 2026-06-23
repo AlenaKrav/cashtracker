bb505b56a28c861c3b84ceed297ecb89c7b37e80
AlenaKravtsova <babkaroza@gmail.com>
2026-06-23 09:38:40 +0200

    Added function to generate jwt once user successfully logged in, added rate limiter to all auth routes

--- backend/src/controllers/AuthControllers.ts
+++ backend/src/controllers/AuthControllers.ts
@@ -1,8 +1,9 @@
 import type { Request, Response } from "express";
 import User from "../models/User";
-import { hashPassword } from "../utils/auth";
+import { hashPassword, verifyPassword } from "../utils/auth";
 import { generateToken } from "../utils/token";
 import { AuthEmail } from "../emails/AuthEmail";
+import { generateJWT } from "../utils/jwt";
 
 export class AuthController {
   static createAccount = async (req: Request, res: Response) => {
@@ -56,5 +57,33 @@ export class AuthController {
     await user.save();
     //  await user.update({confirmed: true});
     res.status(200).json({ message: "Cuenta confirmada correctamente" });
+  };
+
+  static login = async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+    const existingUser = await User.findOne({
+      where: {
+        email,
+      },
+    });
+
+    if (!existingUser) {
+      const error = new Error("Usuario no encontrado");
+      return res.status(404).json({ error: error.message });
+    }
+
+    if (!existingUser.confirmed) {
+      const error = new Error("La cuenta no ha sido confirmada");
+      return res.status(403).json({ error: error.message });
+    }
+
+    const verifiedPass = await verifyPassword(password, existingUser.password);
+    if (!verifiedPass) {
+      const error = new Error("Password incorrecto");
+      return res.status(401).json({ error: error.message });
+    }
+    const token = generateJWT(existingUser.id)
+    res.json(token)
+    res.status(200).json({ message: "Logueado correctamente" });
   };
 }
--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -7,24 +7,37 @@ import { limiter } from "../config/rateLimiter";
 const router = Router();
 
 //para limitar las peticiones solo en ese router (solo las peticiones auth)
-// router.use(limiter)
+router.use(limiter)
 
-router.post('/create-account',
-    body('name')
-    .notEmpty().withMessage('Debes introducir el nombre de usuario'),
-    body('password')
-    .isLength({min: 8}).withMessage('La contraseña debe tener mínimo 8 caracteres'),
-    body('email')
-    .isEmail().withMessage('E-mail no válido'),
+router.post(
+  "/create-account",
+  body("name").notEmpty().withMessage("Debes introducir el nombre de usuario"),
+  body("password")
+    .isLength({ min: 8 })
+    .withMessage("La contraseña debe tener mínimo 8 caracteres"),
+  body("email").isEmail().withMessage("E-mail no válido"),
+  handleInputErrors,
+  AuthController.createAccount,
+);
+
+router.post(
+  "/confirm-account",
+   //limiter solo ponemos limite en este endpoint concreto
+  body("token")
+    .notEmpty()
+    .isLength({ min: 6, max: 6 })
+    .withMessage("Token no válido"),
+  handleInputErrors,
+  AuthController.confirmAccount,
+);
+
+
+router.post("/login",
+    body("email").isEmail().withMessage("E-mail no válido"),
+    body("password")
+    .notEmpty().withMessage("El password es obligatorio"),
     handleInputErrors,
-    AuthController.createAccount);
-
-
-router.post('/confirm-account',
-    limiter, //solo ponemos limite en este endpoint concreto
-    body('token').notEmpty().isLength({min: 6, max: 6}).withMessage('Token no válido'),
-    handleInputErrors,
-    AuthController.confirmAccount
+    AuthController.login
 )
 
 export default router;
